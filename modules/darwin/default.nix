74963189321d700e481b47b57f8c35769403eecb
philingood <98781376+philingood@users.noreply.github.com>
2024-11-13 12:29:44 +0300

    hotfix

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,4 +1,10 @@
 { pkgs, username, ... }: {
+  users.users.${username} = {
+    home =
+      if pkgs.stdenvNoCC.isDarwin
+      then "/Users/${username}"
+      else "/home/${username}";
+  };
   imports = [
     ./pam.nix # enableSudoTouchIdAuth is now in nix-darwin, but without the reattach stuff for tmux
     ./core.nix
