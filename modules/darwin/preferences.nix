78f1acffac2ed65b43c9bc391c18ee9aa5a7ec5f
philingood <98781376+philingood@users.noreply.github.com>
2024-11-14 10:36:24 +0300

    deleted some prefs

--- modules/darwin/preferences.nix
+++ modules/darwin/preferences.nix
@@ -164,46 +164,6 @@ _: {
         location = "~/Pictures/Screenshots";
         type = "png";
       };
-      "com.apple.Safari" = {
-        # Privacy: don’t send search queries to Apple
-        UniversalSearchEnabled = false;
-        SuppressSearchSuggestions = true;
-        # Press Tab to highlight each item on a web page
-        WebKitTabToLinksPreferenceKey = true;
-        ShowFullURLInSmartSearchField = true;
-        # Prevent Safari from opening ‘safe’ files automatically after downloading
-        AutoOpenSafeDownloads = false;
-        ShowFavoritesBar = false;
-        IncludeInternalDebugMenu = true;
-        IncludeDevelopMenu = true;
-        WebKitDeveloperExtrasEnabledPreferenceKey = true;
-        WebContinuousSpellCheckingEnabled = true;
-        WebAutomaticSpellingCorrectionEnabled = false;
-        AutoFillFromAddressBook = false;
-        AutoFillCreditCardData = false;
-        AutoFillMiscellaneousForms = false;
-        WarnAboutFraudulentWebsites = true;
-        WebKitJavaEnabled = false;
-        WebKitJavaScriptCanOpenWindowsAutomatically = false;
-        "com.apple.Safari.ContentPageGroupIdentifier.WebKit2TabsToLinks" = true;
-        "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" = true;
-        "com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled" = false;
-        "com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabled" = false;
-        "com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabledForLocalFiles" = false;
-        "com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically" = false;
-      };
-      "com.apple.mail" = {
-        # Disable inline attachments (just show the icons)
-        DisableInlineAttachmentViewing = true;
-        ShouldShowUnreadMessagesInBold = true;
-        ShowActivity = false;
-        ShowBccHeader = true;
-        ShowCcHeader = true;
-        ShowComposeFormatInspectorBar = true;
-        NSUserKeyEquivalents = {
-          Send = "@\\U21a9";
-        };
-      };
       "com.apple.ActivityMonitor" = {
         OpenMainWindow = true;
         IconType = 5; # visualize cpu in dock icon
